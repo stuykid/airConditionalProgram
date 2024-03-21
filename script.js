a3be1d46ad7c11a491ee849f0662803317947f7d
sheriff legrand <sheriff@sheriffs-Air.lan>
2024-03-21 12:32:25 -0400

    add html and css

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const isWeekday = false;
+const isWeekday = true;
 const airConditioning = !true;
+const air = document.getElementById("air");
 
 console.log("Weekday");
 console.log(isWeekday);
 console.log("Air conditioning");
 console.log(airConditioning);
+
+// if (isWeekday) {
+//     air.innerHTML = "AC up and running";
+// } else {
+//     air.innerHTML = "AC will be turned off"
+// }
+
+(isWeekday) 
+? air.innerHTML = "ON"
+: air.innerHTML = "OFF";
+
+
